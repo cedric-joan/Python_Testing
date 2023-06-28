8ccc05d3f1d57a6c717877cb44d9bc572f840ff5
cecilien <cecilien.joan@gmail.com>
2023-06-28 17:08:31 +0200

    correction of bug email

--- server.py
+++ server.py
@@ -26,9 +26,16 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
-
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except IndexError:
+        if request.form['email'] == "":
+            flash("please enter your email")
+            return render_template('index.html')
+        else:
+            flash("No account related to this email")
+            return render_template('index.html')
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
