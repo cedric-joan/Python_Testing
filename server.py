d7b1359a87a04dc84b880a4facdad39de7582587
cecilien <cecilien.joan@gmail.com>
2023-07-18 15:35:20 +0200

    delete test loading data

--- server.py
+++ server.py
@@ -38,14 +38,17 @@ def showSummary():
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
-
+    try:
+        foundClub = [c for c in clubs if c['name'] == club][0]
+        foundCompetition = [c for c in competitions if c['name'] == competition][0]
+        if foundClub and foundCompetition:
+            return render_template('booking.html',club=foundClub,competition=foundCompetition), 200
+        else:
+            flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=competitions)
+    except IndexError:
+        flash('Please try again !')
+    return render_template("welcome.html", club=club, competitions=competitions), 403
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
