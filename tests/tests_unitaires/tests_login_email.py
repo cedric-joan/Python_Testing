167d41782465038f7cb43ba7efbb0a28d8cc1831
cecilien <cecilien.joan@gmail.com>
2023-07-18 15:08:49 +0200

    insert new assertions

--- tests/tests_unitaires/tests_login_email.py
+++ tests/tests_unitaires/tests_login_email.py
@@ -5,11 +5,15 @@ client = app.test_client()
 def test_is_empty_email():
     result = client.post('/showSummary', data={'email': ''})
     assert result.status_code == 401
+    assert b'Please enter a valid email'
 
 def test_invalid_email():
     result = client.post('/showSummary', data={'email': 'john.doe@graveyard.com'})
     assert result.status_code == 401
+    assert b'Please enter a valid email'
 
 def test_is_valid_email():
     result = client.post('/showSummary', data={'email': 'admin@irontemple.com'})
     assert result.status_code == 200
+    assert b'Welcome, admin@irontemple.com'
+
