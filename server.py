204a8002851f70cc0d0cfe8e07f290f8bb9e0c33
cecilien <cecilien.joan@gmail.com>
2023-07-26 14:13:07 +0200

    update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for
 
+
 def loadClubs():
     with open('clubs.json') as c:
          listOfClubs = json.load(c)['clubs']
@@ -20,6 +21,7 @@ def init_club_bookings(list_of_clubs, list_of_competitions):
 
     return dict_bookings
 
+
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
@@ -27,20 +29,23 @@ competitions = loadCompetitions()
 clubs = loadClubs()
 bookings = init_club_bookings(clubs, competitions)
 
+
 @app.route('/')
 def index():
     return render_template('index.html'), 200
+
 
 @app.route('/show-summary', methods=['POST'])
 def showSummary():
     try:
         club = [club for club in clubs if club['email'] == request.form['email']][0]
-        return render_template('welcome.html',club=club,competitions=competitions), 200
+        return render_template('welcome.html', club=club, competitions=competitions), 200
     except IndexError:
         if request.form['email'] == " ":
             return render_template('index.html'), 401
         else:
             return render_template('index.html', error_message="Please enter a valid email"), 401
+
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
@@ -48,7 +53,7 @@ def book(competition,club):
         foundClub = [c for c in clubs if c['name'] == club][0]
         foundCompetition = [c for c in competitions if c['name'] == competition][0]
         if foundClub and foundCompetition:
-            return render_template('booking.html',club=foundClub,competition=foundCompetition), 200
+            return render_template('booking.html', club=foundClub, competition=foundCompetition), 200
         else:
             flash("Something went wrong-please try again")
             return render_template('welcome.html', club=club, competitions=competitions), 400
@@ -56,7 +61,8 @@ def book(competition,club):
         flash("Something went wrong-please try again")
     return render_template('welcome.html', club=club, competitions=competitions), 404
 
-@app.route('/purchase-places',methods=['POST'])
+
+@app.route('/purchase-places', methods=['POST'])
 def purchasePlaces():
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
@@ -86,10 +92,12 @@ def get_competition_club(competition_name, club_name):
 def get_bookings(club_name, competition_name):
     return bookings[club_name][competition_name]
 
+
 @app.route('/clubs')
 def view_club_points():
     club_list = sorted(clubs, key=lambda club: club['name'])
     return render_template('list_clubs.html', clubs=club_list)
+
 
 @app.route('/logout')
 def logout():
